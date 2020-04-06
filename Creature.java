ab80c5a76d8a32b2a2a3af4cc90146951970c817
g00197439 <60353410+g00197439@users.noreply.github.com>
2020-04-06 11:26:45 -0400

    Set up the Creature class

--- Creature.java
+++ Creature.java
@@ -1,22 +1,69 @@
+import java.util.Random;
 
 /**
  * Write a description of class Creature here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Ryan Boehm 
+ * @version 4/6/2020
  */
 public class Creature
 {
    private int hp;
    private int strength;
+   private Random r;
    
-   public Creature(){
-       
+   /**
+    * Basic constructor for the Creature class
+    */
+   public Creature() {
+       hp = 10;
+       strength = 10;
+       r = new Random();
    }
-    
-   public int damage(){
-       //TODO: change this
-       return 0;
-    }
-    
+   
+   /**
+    * Constructor for the Creature class. This constructor allows for
+    * unique values to be added into the Creature rather than 10.
+    */
+   public Creature(int hitPoints, int power) {
+       hp = hitPoints;
+       strength = power;
+       r = new Random();
+   }
+   
+   /**
+    * Returns a random value between 1 and the creature's strength
+    * @return A random damage
+    */
+   public int damage() {
+       return r.nextInt(strength) + 1;
+   }
+   
+   /**
+    * Checks to see if the creature has more than 0 hit points
+    * @return True if creature has more than 0 hit points and false otherwise
+    */
+   public boolean isAlive() {
+       if(hp > 0) {
+           return true;
+       } else {
+           return false;
+       }
+   }
+   
+   /**
+    * Returns the strength of the creature
+    * @return The strength of the creature
+    */
+   public int getStrength() {
+       return strength;
+   }
+   
+   /**
+    * Returns the current HP of the creature
+    * @return The currernt HP of the creature
+    */
+   public int getHp() {
+       return hp;
+   }
 }
